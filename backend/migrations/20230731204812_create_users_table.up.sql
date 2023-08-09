75905dab3e68c00cd9cbe4695ebff1d6bb8f2baf
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-08-09 12:40:07 -0700

    updated users table with ban

--- backend/migrations/20230731204812_create_users_table.up.sql
+++ backend/migrations/20230731204812_create_users_table.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS users
     id  serial PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN DEFAULT FALSE
+    is_admin BOOLEAN DEFAULT FALSE,
+    is_banned BOOLEAN DEFAULT FALSE
 )
