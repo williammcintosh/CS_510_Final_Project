aa1acc323b996ace8f4e1bcf0fcc5d97ef41e5f9
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-08-11 17:16:42 -0700

    Added seed user

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,7 +5,9 @@ mod api_calls;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
 
-    api_calls::get_all_apods().await?;
+    api_calls::post_user("new@cl.ent", "pass1234", "pass1234").await?;
+
+    // api_calls::get_all_apods().await?;
 
     // api_calls::get_all_questions("1").await?;
 
