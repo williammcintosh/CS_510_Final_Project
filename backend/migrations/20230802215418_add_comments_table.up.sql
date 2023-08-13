b0172ceb32e2d1dcb115d8b35fbf73902a4cc7c3
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-08-13 14:15:24 -0700

    seeded comments. removed apod_id

--- backend/migrations/20230802215418_add_comments_table.up.sql
+++ backend/migrations/20230802215418_add_comments_table.up.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS comments
    id                     serial PRIMARY KEY,
     content                TEXT      NOT NULL,
     created_on             TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    question_id            integer REFERENCES questions ON DELETE CASCADE,
-    apod_id                integer REFERENCES apods ON DELETE CASCADE
+    question_id            integer REFERENCES questions ON DELETE CASCADE
 );
