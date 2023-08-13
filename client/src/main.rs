8e240087fe16fc2f0af5de6352b1cc00d36b86eb
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-08-13 09:06:00 -0700

    Seeded users works

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,7 +5,10 @@ mod api_calls;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
 
-    api_calls::get_all_apods().await?;
+    // api_calls::get_all_apods().await?;
+
+    api_calls::post_user("first@site.com", "1234qwer", "1234qwer").await?;
+    api_calls::post_user("second@site.com", "1234qwer", "1234qwer").await?;
 
     // api_calls::get_all_questions("1").await?;
 
